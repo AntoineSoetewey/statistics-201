83784a4fcb5e7ab1fd0cd725f0f3dcaa61ee6ee4
Antoine Soetewey <ant.soetewey@gmail.com>
2020-03-09 11:51:13 +0100

    edit app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # statistics-201
-Shiny app to perform statistical inference on mean(s), proportion(s) and variance(s). More information: https://antoinesoetewey.shinyapps.io/statistics-201/
+Shiny app to perform statistical inference on mean(s), proportion(s) and variance(s). More information: https://antoinesoetewey.shinyapps.io/statistics-201/ or https://www.statsandr.com/blog/a-shiny-app-for-inferential-statistics-by-hand/
--- app.R
+++ app.R
@@ -237,7 +237,7 @@ ui <- fluidPage(
         value = 0.05
       ),
       hr(),
-      HTML('<p>Report a <a href="https://github.com/AntoineSoetewey/statistics-201/issues">bug</a> or view the <a href="https://github.com/AntoineSoetewey/statistics-201/blob/master/app.R">code</a>. Back to <a href="https://www.antoinesoetewey.com/">www.antoinesoetewey.com</a>.</p>'),
+      HTML('<p>Report a <a href="https://github.com/AntoineSoetewey/statistics-201/issues">bug</a> or view the <a href="https://github.com/AntoineSoetewey/statistics-201/blob/master/app.R">code</a>. Back to <a href="https://www.antoinesoetewey.com/">www.antoinesoetewey.com</a> or <a href="https://www.statsandr.com/">www.statsandr.com</a>.</p>'),
       hr(),
       # HTML('<hr style="border:1px solid #ccc;"/>'),
       HTML('<a rel="license" href="http://creativecommons.org/licenses/by/2.0/be/" target="_blank"><img alt="Licence Creative Commons" style="border-width:0"
